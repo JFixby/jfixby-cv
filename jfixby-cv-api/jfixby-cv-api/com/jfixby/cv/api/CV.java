d82c3b981f27e66d45efc678e146e8a05abd3306
J Fixby <github@jfixby.com>
2017-03-11 11:34:20 +0100

    ....

--- jfixby-cv-api/jfixby-cv-api/com/jfixby/cv/api/CV.java
+++ jfixby-cv-api/jfixby-cv-api/com/jfixby/cv/api/CV.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.cv.api;
 
 import com.jfixby.scarabei.api.ComponentInstaller;
@@ -9,48 +10,47 @@ import com.jfixby.scarabei.api.image.ColoredλImage;
 
 public class CV {
 
-    static private ComponentInstaller<CVComponent> componentInstaller = new ComponentInstaller<CVComponent>("CV");
+	static private ComponentInstaller<CVComponent> componentInstaller = new ComponentInstaller<CVComponent>("CV");
 
-    public static final void installComponent(CVComponent component_to_install) {
-	componentInstaller.installComponent(component_to_install);
-    }
+	public static final void installComponent (final CVComponent component_to_install) {
+		componentInstaller.installComponent(component_to_install);
+	}
 
-    public static final CVComponent invoke() {
-	return componentInstaller.invokeComponent();
-    }
+	public static final CVComponent invoke () {
+		return componentInstaller.invokeComponent();
+	}
 
-    public static final CVComponent component() {
-	return componentInstaller.getComponent();
-    }
+	public static final CVComponent component () {
+		return componentInstaller.getComponent();
+	}
 
-    public static ColoredλImage grayScale(ColoredλImage input) {
-	return invoke().grayScale(input);
-    }
+	public static ColoredλImage grayScale (final ColoredλImage input) {
+		return invoke().grayScale(input);
+	}
 
-    public static ColoredλImage invert(ColoredλImage input) {
-	return invoke().invert(input);
-    }
+	public static ColoredλImage invert (final ColoredλImage input) {
+		return invoke().invert(input);
+	}
 
-    public static ColoredλImage blur(ColoredλImage input, float radius, float image_width, float image_height) {
-	return invoke().blur(input, radius, image_width, image_height);
-    }
+	public static ColoredλImage blur (final ColoredλImage input, final float radius, final float image_width,
+		final float image_height) {
+		return invoke().blur(input, radius, image_width, image_height);
+	}
 
-    public static void averageColor(Collection<Color> collectedColors, CustomColor average) {
-	invoke().averageColor(collectedColors, average);
-    }
+	public static void averageColor (final Collection<Color> collectedColors, final CustomColor average) {
+		invoke().averageColor(collectedColors, average);
+	}
 
-    public static ColoredλImage scale(ColoredλImage λimage, float scalefactor) {
-	return invoke().scale(λimage, scalefactor);
-    }
+	public static ColoredλImage scale (final ColoredλImage λimage, final float scalefactor) {
+		return invoke().scale(λimage, scalefactor);
+	}
 
-    public static ColoredλImage scale(ColoredλImage λimage, float scaleX, float scaleY) {
-	return invoke().scale(λimage, scaleX, scaleY);
-    }
+	public static ColoredλImage scale (final ColoredλImage λimage, final float scaleX, final float scaleY) {
+		return invoke().scale(λimage, scaleX, scaleY);
+	}
 
-    public static ColoredλImage map(ColoredλImage λimage, Rectangle inputArea, Rectangle outputArea) {
-	return invoke().map(λimage, inputArea, outputArea);
-    }
-
-    
+	public static ColoredλImage map (final ColoredλImage λimage, final Rectangle inputArea, final Rectangle outputArea) {
+		return invoke().map(λimage, inputArea, outputArea);
+	}
 
 }
